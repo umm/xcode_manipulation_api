d8f86911e1d82d787f31573b96e7e42fd2c92b6f
Tetsuya Mori <monry84@gmail.com>
2018-03-06 10:46:11 +0900

    :recycle: Fix obsoleted

--- Assets/Scripts/UnityModule/iOS/Xcode/PlistParser.cs
+++ Assets/Scripts/UnityModule/iOS/Xcode/PlistParser.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -148,7 +148,7 @@ namespace UnityModule.iOS.Xcode
         internal static XDocument ParseXmlNoDtd(string text)
         {
             XmlReaderSettings settings = new XmlReaderSettings();
-            settings.ProhibitDtd = false;
+            settings.DtdProcessing = DtdProcessing.Ignore;
             settings.XmlResolver = null; // prevent DTD download
 
             XmlReader xmlReader = XmlReader.Create(new StringReader(text), settings);
